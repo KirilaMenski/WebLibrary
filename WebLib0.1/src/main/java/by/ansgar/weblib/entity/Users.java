f12831e42564a219aa6603ba4ab4b4463d603390
Mensky <bv_1514@mail.ru>
2015-09-03 20:53:48 +0300

    Added new column

--- WebLib0.1/src/main/java/by/ansgar/weblib/entity/Users.java
+++ WebLib0.1/src/main/java/by/ansgar/weblib/entity/Users.java
@@ -1,32 +1,63 @@
 package by.ansgar.weblib.entity;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="USERS_WEB")
+@Table(name = "USERS_WEB")
 public class Users {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "ID")
 	private int id;
-	
+
 	@Column(name = "FIRST_NAME")
 	private String firstName;
-	
+
 	@Column(name = "LAST_NAME")
 	private String lastName;
-	
+
+	@Column(name = "login")
+	private String login;
+
 	@Column(name = "PASSWORD")
 	private String password;
-	
+
 	@Column(name = "EMAIL")
 	private String email;
+	@ManyToMany
+	@JoinTable(name = "users_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private List<Role> roles;
+	@Enumerated(EnumType.STRING)
+	private UserStatus status;
+
+	public Users() {
+
+	}
+
+	public Users(int id, String firstName, String lastName, String login,
+			String password, String email, List<Role> roles, UserStatus status) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.login = login;
+		this.password = password;
+		this.email = email;
+		this.roles = roles;
+		this.status = status;
+	}
 
 	public int getId() {
 		return id;
@@ -66,6 +97,30 @@ public class Users {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
+	public UserStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(UserStatus status) {
+		this.status = status;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
 	}
 
 }
