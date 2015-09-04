af0ff2f6bff63c3dda712bb46a8805d257d53716
Mensky <bv_1514@mail.ru>
2015-09-04 15:27:42 +0300

    Some problem with servlet

--- WebLib0.1/src/main/java/by/ansgar/weblib/servlet/RegistrServlet.java
+++ WebLib0.1/src/main/java/by/ansgar/weblib/servlet/RegistrServlet.java
@@ -16,6 +16,7 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 import by.ansgar.weblib.DAO.Factory;
+import by.ansgar.weblib.entity.UserStatus;
 import by.ansgar.weblib.entity.Users;
 
 @WebServlet("/RegistrServlet")
@@ -54,24 +55,27 @@ public class RegistrServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-
+		System.out.println("I am here!");
 		String data = getMessageBody(request);
 		try {
 			JSONObject json = getJSONObject(data);
 
 			users.setFirstName((String) json.get("name"));
 			users.setLastName((String) json.get("lastname"));
+			users.setLogin((String) json.get("login"));
 			users.setPassword((String) json.get("password"));
 			users.setEmail((String) json.get("email"));
+			users.setStatus(UserStatus.ACTIVE);
 			Factory.getInstance().getUsersDAO().addUsers(users);
 
 			List<Users> users = Factory.getInstance().getUsersDAO().users();
 
 			for (int i = 0; i < users.size(); i++) {
-				System.out.println(users.get(i).getId() + " "
-						+ users.get(i).getFirstName() + " "
-						+ users.get(i).getLastName() + " "
-						+ users.get(i).getPassword() + " "
+				System.out.println(users.get(i).getId() + "\t"
+						+ users.get(i).getFirstName() + "\t"
+						+ users.get(i).getLastName() + "\t"
+						+ users.get(i).getLogin() + "\t"
+						+ users.get(i).getPassword() + "\t"
 						+ users.get(i).getEmail());
 			}
 
